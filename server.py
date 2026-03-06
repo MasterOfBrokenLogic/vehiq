af71b9e4e34275c4606ebfc7a49253ca5d3247f9
DrazeForce <drazeforce.io@gmil.com>
2026-03-07 01:11:56 +0530

    Just changed a line of code!!!

--- server.py
+++ server.py
@@ -96,4 +96,4 @@ if __name__ == "__main__":
   ║  Install openssl to enable HTTPS automatically.  ║
   ╚══════════════════════════════════════════════════╝
         """)
-        app.run(host="0.0.0.0", port=5000, debug=False)
+        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
